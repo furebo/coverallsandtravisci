ba6afe05528f0e20560594cbe6997b0ed5dbd6c3
Furebo <furebodidace582@gmail.com>
2023-04-09 10:55:15 +0200

    changed from ES6 to ES5

--- aboutnumbers.js
+++ aboutnumbers.js
@@ -1,5 +1,5 @@
-const operations = {
-    sum : (x,y) => {
+module.exports = {
+     sum :(x,y)=> {
       return x + y;
        
      },
@@ -12,4 +12,5 @@ const operations = {
 
 }
 
-  export default operations;
+
+  
